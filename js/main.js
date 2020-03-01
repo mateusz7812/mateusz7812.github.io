e7560826580f7c6e9fd0eb85ebe169adc14b9f11
mateusz7812 <mateusz78122@gmail.com>
2020-03-01 18:10:32 +0100

    text data changed

--- js/main.js
+++ js/main.js
@@ -14,6 +14,16 @@ function scrollToId(id){
     behavior: 'smooth'
   });
   }
+
+function mapsSelector() {
+  if /* if we're on iOS, open in Apple Maps */
+  ((navigator.platform.indexOf("iPhone") != -1) ||
+    (navigator.platform.indexOf("iPad") != -1) ||
+    (navigator.platform.indexOf("iPod") != -1))
+    window.open("maps://maps.google.com/maps?daddr=<lat>,<long>&amp;ll=");else /* else use Google */
+    window.open("https://maps.google.com/maps?daddr=<lat>,<long>&amp;ll=");
+}
+
 function sendEmail() {
   window.open('mailto:test@example.com?subject=subject&body=body', "_self");
 }
